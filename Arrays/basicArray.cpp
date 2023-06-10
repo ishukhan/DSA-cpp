cf5d58a0f02bf32c942518d021ccde5f5c80b2dd
ishukhan <ishukk00@gmail.com>
2023-06-10 10:29:22 +0530

    Arrays

---
+++ Arrays/basicArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 1D Array
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    // cout << arr[2];
+
+    // 2D Array
+    // int arr[3][5];
+
+    // arr[1][3] = 80;
+    // cout << arr[1][3];
+
+    // Stirng Array;
+
+    string i = "ishukhan";
+    int len = i.size();
+    i[len - 1] = '2';
+    cout << i[len - 1];
+}
